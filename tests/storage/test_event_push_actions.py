ee9dd4b5d7e49f5b6841d4eec58c6d7820146c1d
Patrick Cloke <patrickc@matrix.org>
2022-06-17 13:47:55 -0400

    debug

--- tests/storage/test_event_push_actions.py
+++ tests/storage/test_event_push_actions.py
@@ -58,6 +58,7 @@ class EventPushActionsStoreTestCase(HomeserverTestCase):
         user_id = "@user1235:test"
 
         last_read_stream_ordering = [0]
+        desc = ["A"]
 
         def _assert_counts(noitf_count: int, highlight_count: int) -> None:
             counts, thread_counts = self.get_success(
@@ -121,6 +122,10 @@ class EventPushActionsStoreTestCase(HomeserverTestCase):
                 )
             )
 
+            self.get_success(
+                self.store.db_pool.runInteraction("foo", f, "_inject_actions")
+            )
+
         def _rotate(stream: int) -> None:
             self.get_success(
                 self.store.db_pool.runInteraction(
@@ -134,6 +139,8 @@ class EventPushActionsStoreTestCase(HomeserverTestCase):
                 )
             )
 
+            self.get_success(self.store.db_pool.runInteraction("foo", f, "_rotate"))
+
         def _mark_read(stream: int, depth: int) -> None:
             last_read_stream_ordering[0] = stream
 
@@ -146,6 +153,19 @@ class EventPushActionsStoreTestCase(HomeserverTestCase):
                     data={},
                 )
             )
+
+            self.get_success(self.store.db_pool.runInteraction("foo", f, "_mark_read"))
+
+        def f(txn, func):
+            print(f"\n{desc[0]}: {func}")
+            txn.execute("SELECT * FROM event_push_summary")
+            print([x[:-1] for x in txn.fetchall()])
+
+            txn.execute("SELECT * FROM event_push_actions")
+            print([x[:-1] for x in txn.fetchall()])
+
+            desc[0] = chr(ord(desc[0]) + 1)
+            print()
 
         _assert_counts(0, 0)
         _inject_actions(1, PlAIN_NOTIF)
@@ -166,6 +186,7 @@ class EventPushActionsStoreTestCase(HomeserverTestCase):
         _assert_counts(0, 0)
 
         _inject_actions(6, PlAIN_NOTIF)
+        _assert_counts(1, 0)
         _rotate(7)
         _assert_counts(1, 0)
 
